e0928282e34515e8a27077de6f83655295372d57
Rob C <rob.charlish@gmail.com>
2025-01-16 11:34:39 +0000

    FixMe done

---
+++ Assets/Week 1/1. FixMe Tutorial/FixMeScript.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class FixMeScript : MonoBehaviour
+{
+
+    public Image bg;
+    public GameObject congratsText;
+    public TMP_Text Add1text;
+    public TMP_Text RandomNameText;
+    public Toggle ColorChange;
+    private int Add1Number;
+    private float timer;
+    public string[] names =    {"Sandra Key",
+                                "Chaz Novak",
+                                "Reid Nash",
+                                "Baron Allison",
+                                "German Rodgers",
+                                "Dustin Carey",
+                                "Thalia Stokes",
+                                "Nathalie Bruce",
+                                "Cindy Burch",
+                                "Brisa Moore",
+                                "Genesis Levine",
+                                "Madeleine Cohen",
+                                "Allyson Rosales",
+                                "Jacquelyn Williamson",
+                                "Abdullah Norman",
+                                "Tabitha Gillespie",
+                                "Rodrigo Hunt",
+                                "Hayley Crosby",
+                                "Yesenia Eaton",
+                                "Lena Herman"
+                                };
+
+    void Awake() 
+    {
+        congratsText.transform.localScale = new Vector2(0,0);
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 newScale = new Vector3 (1,1,1);
+        StartCoroutine(LerpScale(newScale, 1f));
+        Add1text.text = Add1Number.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (ColorChange.isOn == true && timer == 200f)
+        {
+            ChangeSkybox();
+        }
+
+        timer ++;
+
+        if (timer > 200f){
+            timer = 0;
+        }
+
+
+    }
+
+    IEnumerator LerpScale(Vector3 targetLocalScale, float duration)
+    {
+        var startScale = congratsText.transform.localScale;
+
+        for(var timePassed = 0f; timePassed < duration; timePassed += Time.deltaTime)
+        {
+            var factor = timePassed / duration;
+            factor = Mathf.SmoothStep(0, 1, factor);
+
+            congratsText.transform.localScale = Vector3.Lerp(startScale, targetLocalScale, factor);
+
+            yield return null;
+        }
+
+        congratsText.transform.localScale = targetLocalScale;
+    }
+
+    public void Add1Button() 
+    {
+        Add1Number ++;
+        Add1text.text = Add1Number.ToString();
+    }
+
+    public void PickAName()
+    {
+        RandomNameText.text = names[Random.Range(0, names.Length)];
+    }
+
+    void ChangeSkybox()
+    {
+       bg.color = new Color (Random.Range(0, 1f), Random.Range(0, 1f), Random.Range(0, 1f));
+        
+    }
+
+}
