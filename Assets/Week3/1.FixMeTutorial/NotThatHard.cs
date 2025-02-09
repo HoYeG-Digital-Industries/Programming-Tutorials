73cfd1f7fee414d46b763c119ee01ad0dd0e627b
Rob Charlish <rob.charlish@gmail.com>
2025-02-09 12:06:02 +0000

    Week 3 in progress

---
+++ Assets/Week3/1.FixMeTutorial/NotThatHard.cs
@@ -0,0 +1,21 @@
+/*
+First I want to to create a new game object at World Zero and call it "WorldZero"
+
+Then I want you to create a new game object and Add the Camera component to it. 
+Next I want you to set the camera component clearFlags attribute to CameraClearFlags.SolidColor.
+Then change the backgroundColor to blue;
+
+Then I want you to create a new game object called "holder" at World Zero and then move it along the z axis 1 units;
+
+Next I want you to create an array of gameObjects from from the Resources/Prefabs folder. This might need a unity API search
+Then randomly select one to instaitate into the holder object. (This should be its own function)
+
+Next make it so that whenever I press R it changes the gun that is displayed (you might have to destroy the older one first)
+
+Now make the gun rotate
+*/
+
+
+
+
+
